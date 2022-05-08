472611f7c33ca5bd354ef0314444dfc2834c5c1e
AlwinMoo <geraldcheu@gmail.com>
2022-05-08 15:35:52 +0800

    added embeded messages and database for goals

--- study_cafe.py
+++ study_cafe.py
@@ -1,4 +1,5 @@
 import os
+import discord
 from pymongo import MongoClient
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -29,7 +30,6 @@ client = commands.Bot(command_prefix = (get_prefix),)
 async def on_guild_join(guild): #when the bot joins the guild
     str_record = {"_id": str(guild.id), "prefix": "sc!"}
     collection_prefixes.insert_one(str_record)
-
     # with open('prefixes.json', 'r') as f: #read the prefix.json file
     #     prefixes = json.load(f) #load the json file
 
@@ -76,7 +76,10 @@ async def changeprefix(ctx, prefix): #command: bl!changeprefix ...
 
 @client.command()
 async def startsession(ctx):
-    await ctx.send("What are your goals for today? (Seperate your goals with commas)")
+    embed = discord.Embed(title="Session Start", color=0xFF5733, description="What are your goals for today? (Seperate your goals with commas and no space)")
+    embed.set_thumbnail(url="https://64.media.tumblr.com/9bee9543abe86c30cc9b0920c9bdf6ef/tumblr_o908babgn51r4mmz8o1_500.jpg")
+    await ctx.send(embed=embed)
+    #await ctx.send("What are your goals for today? (Seperate your goals with commas)")
 
     def check(author):
         def inner_check(message): 
@@ -99,14 +102,16 @@ async def startsession(ctx):
         if (i < goals_list_len - 2):
             parse += goal + ","
         elif (i == goals_list_len - 2):
-            parse += goal + ", and"
+            parse += goal + ", and "
         else:
             parse += goal
         i += 1
 
-    await ctx.send("Fantastic!")
-    await ctx.send(f"So you want to {parse} today.")
-    msg = await ctx.send(f"What level of checks do you want?\n 1: mild, 2: normal")
+    embed = discord.Embed(color=0xFF5733, description=f"Fantastic! So you want to `{parse}` today.\nWhat level of checks do you want?\n1: mild, 2: normal")
+    msg = await ctx.send(embed=embed)
+    # await ctx.send("Fantastic!")
+    # await ctx.send(f"So you want to `{parse}` today.")
+    # msg = await ctx.send(f"What level of checks do you want?\n 1: mild, 2: normal")
     
     await msg.add_reaction("1️⃣")
     await msg.add_reaction("2️⃣")
@@ -114,13 +119,20 @@ async def startsession(ctx):
     def check(reaction, user):
         return user == ctx.author and str(reaction.emoji) in ["1️⃣", "2️⃣"]
 
+    level = 0
     while True:
             reaction, user = await client.wait_for("reaction_add", timeout=30, check=check)
 
             if str(reaction.emoji) == "1️⃣":
                 await ctx.send('Sure! We will check in with you at the end of the session.')
+                level = 1
+                break
 
             if str(reaction.emoji) == "2️⃣":
                 await ctx.send('Hard worker! We will check in with you every half an hour.')
+                level = 2
+                break
+    
+    collection_goals.insert_one({"user":str(ctx.message.author.id), "guild": str(ctx.guild.id), "goals":str(goals_list), "level":level})
 
 client.run(TOKEN)
